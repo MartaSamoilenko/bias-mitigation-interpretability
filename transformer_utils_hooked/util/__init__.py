227120be1dda75a6f8e3d40496ff4a16a6f97af8
MartaSamoilenko <marta.samoilenko@gmail.com>
2026-04-27 20:02:00 +0000

    update

--- transformer_utils_hooked/util/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import module_utils
-from . import python_utils
-
-__all__ = [
-    'module_utils',
-    'python_utils',
-]
--- transformer_utils_hooked/util/python_utils.py
+++
@@ -1,6 +0,0 @@
-def make_print_if_verbose(verbose: bool):
-    def vprint(*args, **kwargs):
-        if verbose:
-            print(*args, **kwargs)
-
-    return vprint
